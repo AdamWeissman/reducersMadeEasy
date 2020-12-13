7d1f0b78d65115af56a9cf4ed5b65443d645da5a
AdamWeissman <adam@comedytragedyepic.com>
2020-12-13 13:59:08 -0500

    max value in objects... either actual value, or the key

--- 02-moreInvolvedReducer.js
+++ 02-moreInvolvedReducer.js
@@ -1,6 +1,6 @@
 // REDUCE FOR LARGEST VALUE IN AN ARRAY
 
-const theArray = [1,100,55,1000,89]
+const theArray = [1,100,55,1000, -20,89]
 
 function largestValueinAnArray(fromThisArray) {
   return fromThisArray.reduce(( maxValue, currentValueToCompare) => {
@@ -9,3 +9,47 @@ function largestValueinAnArray(fromThisArray) {
 }
 
 console.log("largest value in an array using reduce", largestValueinAnArray(theArray))
+
+// REDUCE FOR SMALLEST VALUE IN AN ARRAY
+
+function smallestValueinAnArray(fromThisArray) {
+  return fromThisArray.reduce(( minValue, currentValueToCompare) => {
+    return Math.min(minValue, currentValueToCompare)
+  } )
+}
+
+console.log("smallest value in an array using reduce", smallestValueinAnArray(theArray))
+
+// REDUCE FOR LARGEST KEY WITH THE MAX VALUE
+
+const theObject = {
+  A: 1,
+  B: 3,
+  C: 5,
+  D: 89,
+  E: 55,
+  F: 34,
+  G: 21
+}
+
+function keyWithLargestValueinAnObject(fromThisObject) {
+  return Object.keys(fromThisObject).reduce(( keyWithMaxValue, currentKeyWithValueToCompare) => {
+    if (fromThisObject[keyWithMaxValue] > fromThisObject[currentKeyWithValueToCompare]) {
+      return keyWithMaxValue
+    } else {
+      return currentKeyWithValueToCompare
+    }
+  } )
+}
+
+console.log("key with largest value in an Object", keyWithLargestValueinAnObject(theObject))
+
+// REDUCE FOR THE LARGEST VALUE IN AN OBJECT
+
+function theLargestValueinAnObject(fromThisObject) {
+  return Object.values(fromThisObject).reduce(( maxValue, currentValueToCompare) => {
+    return Math.max(maxValue, currentValueToCompare)
+  } )
+}
+
+console.log("return just the largest value in the object", theLargestValueinAnObject(theObject))
