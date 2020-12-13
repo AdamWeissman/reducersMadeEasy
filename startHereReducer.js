a4c1bb343145c95c240a2b2e4588b4ecbd19b36d
AdamWeissman <adam@comedytragedyepic.com>
2020-12-13 13:22:09 -0500

    basic reduce functions

--- startHereReducer.js
+++ startHereReducer.js
@@ -2,13 +2,20 @@
 
 const theArray =  [2,2,2,2,2] // if summed should add up to 10
 
-function sumTheNumbers(fromThisArray) {
+function sumTheNumbers (fromThisArray) {
   return fromThisArray.reduce((accumulator, valueAtIndex) => {
     return accumulator + valueAtIndex
   })
 }
 
-console.log(sumTheNumbers(theArray))
+function sumTheNumbersWithStartingValueOf10 (fromThisArray) {
+  return fromThisArray.reduce((accumulator, valueAtIndex) => {
+    return accumulator + valueAtIndex
+  }, 10)
+}
+
+console.log("using arrow function", sumTheNumbers(theArray))
+console.log("also arrow function with starting value of 10", sumTheNumbersWithStartingValueOf10(theArray))
 
 
 // ALTERNATE SYNTAX USING "FUNCTION" OUTRIGHT INSTEAD OF "IMPLIED ARROW FUNCTION" AS ABOVE
@@ -19,4 +26,13 @@ function sumTheNumbers2(fromThisArray) {
   })
 }
 
-console.log(sumTheNumbers2(theArray))
+function reduceIntoAnEmptyArray(fromThisArray) {
+  return fromThisArray.reduce(function(accumulator, valueAtIndex) {
+    return (accumulator.concat("using the empty array as a starting value" + " " + valueAtIndex + "\n"))
+  }, [])
+}
+
+
+console.log("using standard function syntax", sumTheNumbers2(theArray))
+console.log("using standard function with a starting array instead of a value \n the point being to send all the values into a new array", reduceIntoAnEmptyArray(theArray))
+
